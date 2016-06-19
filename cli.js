f7acdceb9707967e3e41dd66ac270e9e5c18e18b
CodeDotJS <rushforlinux@gmail.com>
2016-06-19 17:48:04 +0530

    for >=4

--- cli.js
+++ cli.js
@@ -5,33 +5,25 @@
 const ansi = require('ansi-rainbow');
 const chalk = require('chalk');
 
-const colors = require('colors');
-
-colors.setTheme({
-	tell: ['yellow', 'bold']
-});
-
-colors.setTheme({
-	info: ['cyan', 'bold']
-});
+const colors = require('colors/safe');
 
 const argv = require('yargs')
-    .usage('\nUsage: $0 -w 118 -l 37'.info)
+    .usage(colors.cyan.bold('\nUsage: $0 -w 118 -l 37'))
     .describe('w', 'Change width when you resize your console.')
     .describe('l', 'Adjust rainbow\'s height')
     .demand(['w', 'l'])
     .argv;
 
-var colString = '.';
+let colString = '.';
 
 while (colString.length % argv.w !== 0) {
 	colString += ' ';
 }
 
-var l = colString.length;
+const l = colString.length;
 
-var spaces = '';
-var i = 0;
+let spaces = '';
+let i = 0;
 for (; i < l; i++) {
 	spaces += ' ';
 }
@@ -48,8 +40,8 @@ ansi
 console.log(ansi.bg(spaces));
 console.log(chalk.black(ansi.bg(colString)));
 
-var count = 0;
-for (var tempIn = 0; tempIn < argv.l; tempIn++) {
+let count = 0;
+for (let tempIn = 0; tempIn < argv.l; tempIn++) {
 	console.log(ansi.bg(spaces));
 	count += 1;
 }
